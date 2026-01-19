87b29d79467089f8890f9df1e1052c2dfd70eaaa
KUCA Brice <kuca.b@sfeir.com>
2026-01-19 10:51:08 +0100

    feat: Initialie spring boot project

---
+++ goals-tracker-back/src/main/java/com/example/goals_tracker/GoalsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.goals_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalsTrackerApplication.class, args);
+	}
+
+}
