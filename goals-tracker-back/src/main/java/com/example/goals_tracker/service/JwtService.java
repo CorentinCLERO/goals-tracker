4b1262298b9eb6ec51ad463ec333568c4f11cb78
KUCA Brice <kuca.b@sfeir.com>
2026-01-19 16:01:07 +0100

    feat: Implement JwtService with token generation functionality

---
+++ goals-tracker-back/src/main/java/com/example/goals_tracker/service/JwtService.java
@@ -0,0 +1,31 @@
+package com.example.goals_tracker.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class JwtService {
+    
+    private final String secretKey = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+    private final long jwtExpiration = 86400000;
+    
+    public String generateToken(UUID userId) {
+        return Jwts.builder()
+                .subject(userId.toString())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey())
+                .compact();
+    }
+    
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
